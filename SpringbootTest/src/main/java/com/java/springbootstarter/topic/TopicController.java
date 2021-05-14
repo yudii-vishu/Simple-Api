5069175a98e0c690dd43dc9d0abc593b458db789
yudii-vishu <vishalsri4554@gmail.com>
2021-05-14 10:51:03 +0530

    Spring API without DB

---
+++ SpringbootTest/src/main/java/com/java/springbootstarter/topic/TopicController.java
@@ -0,0 +1,47 @@
+package com.java.springbootstarter.topic;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService; 
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopic() {
+		return topicService.getAllTopic();
+	}
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.PUT)
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topicService.updateTopic(id,topic);
+	}
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+}
+
+
+
+
+
+
+
+
+
+
